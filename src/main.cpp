772780f61e5b5eedede41ad4c1575463841ebeb0
assembler-0 <diaviekone13@gmail.com>
2025-05-29 20:43:11 +0700

    add randomness

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,29 @@
 #include "../include/core.hpp"
 #include "../include/CLI11.hpp"
 #include <iostream>
+#include <cstdlib>
+void sse_init(){
+	int upper_lm = 0;
+	int lower_lm = 0;
+	int size = 4
+	std::cout << "Please enter upper limit: ";
+	std::cin >> upper_lm;
+	std::cout << "Please enter lower limit: ";
+	std::cin >> lower_lm;
+	alignas(16) float n1[size];
+	alignas(16) float n2[size];
+	alignas(16) float out[size];
+	for (int i = 0; i < size; ++i){
+		n1[i] = rand() % upper_lm + lower_lm;
+		n2[i] = rand() % upper_lm + lower_lm;
+	}
+	sse(n1, n2, out);
+	std::cout << "Result: ";
+	for (float f : out) std::cout << f << " ";
+	std::cout << std::endl;
+	return;
+}
 int main(){
-  //CLI::App ACTS("Advanced CPU Testing System", "ACTS");
-  alignas(16) float a[4] = {1.0f, 2.0f, 3.0f, 4.0f};
-  alignas(16) float b[4] = {5.0f, 6.0f, 7.0f, 8.0f};
-  alignas(16) float out[4];
-  sse(a, b, out);
-  std::cout << "Result: ";
-  for (float f : out) std::cout << f << " ";
-  std::cout << std::endl;
+  sse_init();
   return 0;
 }
