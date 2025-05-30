5b233632ff569853edf401704cce9c12b2dbcb20
assembler-0 <diaviekone13@gmail.com>
2025-05-30 10:00:55 +0700

    changes

--- asm/core.asm
+++ asm/core.asm
@@ -1,8 +1,8 @@
 global sse
 section .text
 sse:
-        movaps xmm0, [rdi]      ; load 4 floats from ptr1
-        movaps xmm1, [rsi]      ; load 4 floats from ptr2
-        addps  xmm0, xmm1       ; xmm0 = xmm0 + xmm1
-        movaps [rdx], xmm0      ; store result to output ptr
+        vmovaps ymm0, [rdi]
+        vmovaps ymm1, [rsi]
+        vmulps  ymm0, ymm1
+        vmovaps [rdx], ymm0
         ret
--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,17 @@
 #include "../include/core.hpp"
-#include "../include/CLI11.hpp"
 #include <iostream>
 #include <cstdlib>
 void sse_init(){
 	int upper_lm = 0;
 	int lower_lm = 0;
-	int size = 4
+	const int size = 8;
 	std::cout << "Please enter upper limit: ";
 	std::cin >> upper_lm;
 	std::cout << "Please enter lower limit: ";
 	std::cin >> lower_lm;
-	alignas(16) float n1[size];
-	alignas(16) float n2[size];
-	alignas(16) float out[size];
+	alignas(32) float n1[size];
+	alignas(32) float n2[size];
+	alignas(32) float out[size];
 	for (int i = 0; i < size; ++i){
 		n1[i] = rand() % upper_lm + lower_lm;
 		n2[i] = rand() % upper_lm + lower_lm;
