702c280056a3b0b01a84a89dad6892ee9e4d6f4a
assembler-0 <diaviekone13@gmail.com>
2025-06-01 08:58:16 +0700

    update

--- asm/core.asm
+++ asm/core.asm
@@ -4,6 +4,7 @@ avx:
         vmovaps ymm0, [rdi]
         vmovaps ymm1, [rsi]
         vmovaps ymm2, [rdx]
-        vfmadd132ps ymm0, ymm1, ymm2
+        vfmaddsub132ps ymm0, ymm1, ymm2
+        vfmsubadd132ps ymm0, ymm1, ymm2
         vmovaps [rcx], ymm0
         ret
