0b6e6f848edf367c24514ea458a60f41323ef0fd
assembler-0 <diaviekone13@gmail.com>
2025-06-01 16:24:06 +0700

    update

--- asm/3np1.asm
+++ asm/3np1.asm
@@ -1,8 +1,27 @@
 global p3np1E
 section .text
+; rdi = input
+; rsi = out
 p3np1E:
-        xor rsi, rsi
         cmp rdi, 1
-        je .loop_eos_at_1
-.loop_eos_at_1
+        je .loopEosAt1
+        jne .start
+ .start:
+        test rdi, 1
+        jnz .isEven
+        jz .isOdd
+ .isOdd:
+        imul rdi, 3
+        add rdi, 1
+        add rsi, 1
+        jmp .checkLoopCondition
+ .isEven:
+        shr rdi, 1
+        add rsi,1
+        jmp .checkLoopCondition
+ .checkLoopCondition:
+        cmp rdi, 1
+        jne .start
+        je .loopEosAt1
+ .loopEosAt1:
         ret
