c406d10138af0f4b6680b88652583393cb041cdc
assembler-0 <diaviekone13@gmail.com>
2025-06-08 17:17:30 +0700

    Added SHA256 hashing

---
+++ esstAPI.hpp
@@ -0,0 +1,25 @@
+#pragma once
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+    void sha256(long iterations);
+    void initGPU(int iterations);
+    void p3np1E(unsigned long a, unsigned long * steps);
+    void primes(unsigned long a, unsigned long * steps);
+    void avx(float * a, float * b, float * c);
+    void floodL1L2(void* buffer, unsigned long * iterations_ptr, size_t buffer1_size);
+    void floodMemory(void* buffer, unsigned long * iterations_ptr, size_t buffer_size);
+    void rowhammerAttack(void* buffer, unsigned long * iterations_ptr, size_t buffer_size);
+    void floodNt(void * buffer, unsigned long * iterations_ptr, size_t buffer_size);
+    void aes128EncryptBlock(void * out, const void * in, const void * key);
+    void aes256Keygen(void* expanded_key);
+    void aesXtsEncrypt(void * out, const void * in, const void* key, const void * tweak, size_t blocks);
+    void aes128DecryptBlock(void * out, const void * in, const void * key);
+    void aesXtsDecrypt(void * out, const void * in, const void* key, const void * tweak, size_t blocks);
+    // ... (more entries continue)
+
+#ifdef __cplusplus
+}
+#endif
